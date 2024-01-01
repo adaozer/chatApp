c84da8c020efbc006ab552fab9094b877adbcb70
adaozer <ada.a.ozer@durham.ac.uk>
2024-01-01 16:44:22 +0300

    5dkda logging ekledik sadece download kaldı lmfao

--- deneme.py
+++
@@ -1,4 +0,0 @@
-dictionary = {1: 2, 3: 4, 5: 6}
-
-for i in len(dictionary):
-    print(i)
--- server.py
+++ server.py
@@ -1,21 +1,24 @@
 import socket
 import threading
 import sys
+import logging
 
 port = int(sys.argv[1])
 activeClients = {}
+logging.basicConfig(filename='server.log', level=logging.INFO)
 
 # https://www.youtube.com/watch?v=Cqoqd31BbwI&t=1155s&ab_channel=AllAboutPython
 # https://docs.python.org/3/library/socket.html
 # https://python.plainenglish.io/building-a-messaging-app-with-python-sockets-and-threads-1c110fc1c8c8
 # https://python.plainenglish.io/building-a-messaging-app-with-python-sockets-and-threads-continue-b7b344ff6e76
 # https://www.geeksforgeeks.org/simple-chat-room-using-python/
+# https://docs.python.org/3/howto/logging.html
+# https://docs.python.org/3/library/logging.html
 
 def unicast(username, message):
 
     splitMessage = message.split()
-    receiver = splitMessage[1]
-                    
+    receiver = splitMessage[1]            
     # https://www.geeksforgeeks.org/python-get-key-from-value-in-dictionary/
 
     dictKeys = list(activeClients.keys())
@@ -25,8 +28,8 @@ def unicast(username, message):
                     
     messageFinal = " ".join(splitMessage[2:])
     sendUni = username + ": " + messageFinal
+    logging.info(f"{username} sent a private message to {receiver} with the content: {messageFinal}")
     receiverSocket.sendall(sendUni.encode("utf-8"))
-
 
     
 def broadcast(client, message):
@@ -34,10 +37,10 @@ def broadcast(client, message):
         if clients != client: 
             clients.sendall(message.encode('utf-8'))
 
-
 def clientLeave(client):
     username = activeClients[client]
     broadcast(client, f"{username} has left!")
+    logging.info(f"Server broadcasted to the clients that {username} left the server.")
     activeClients.pop(client)
     client.close()
 
@@ -49,7 +52,9 @@ def clientAdd(client, address):
         activeClients[client] = username  
         hello = f"{username} has joined!"
         broadcast(client, hello) 
+        logging.info(f"Server broadcasted to the clients that {username} joined the server.")
         client.sendall(f"Welcome to the server {username}!".encode('UTF-8'))
+        logging.info(f'{username} joined the server from port {address}')
 
     except:
         print("Client could not connect!")
@@ -64,31 +69,40 @@ def clientAdd(client, address):
 
             elif message == "/leave":
                 clientLeave(client)
+                logging.info(f"{username} left the server.")
                 break
 
             else:
                 sendBroad = username + ": " + message
                 broadcast(client, sendBroad)
+                logging.info(f"{username} broadcasted a message with the content: {message}")
 
         except KeyboardInterrupt:
             clientLeave(client) 
+            logging.info(f"{username} left the server.")
             break
 
         except ConnectionResetError:
             clientLeave(client) 
+            logging.error(f"{username} has disconnected unexpectedly.")
             break
 
-        except:
-            print("Message failed to send.")
+       # except:
+        #    errorMessage = "Message failed to send."
+         #   print(errorMessage)
+          #  logging.error(f"A message failed to send from {username}")
+           # client.sendall(errorMessage.encode('utf-8'))
 
 def main():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         try:
             s.bind(("", port))
             print(f"Server has started at {port}")
+            logging.info(f"Server started at {port}")
         except:
             print(f"Server unable to be bound to {port}")
-        
+            logging.critical(f"Server unable to be bound to {port}")
+
         s.listen()
 
         while True:
